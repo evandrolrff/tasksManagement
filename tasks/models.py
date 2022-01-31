46ce5d95087ff194638acde7ae06ad14040e621d
Evandro <evandrolrff@gmail.com>
2022-01-31 15:20:02 -0300

    modelo tarefas e view de listagem

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Information(models.Model):
+    TYPE_CHOICES = [
+        ('B', 'Bug'),
+        ('P', 'Phase'), 
+        ('T', 'Task'), 
+        ('U', 'User story')]
+    TYPE_STATUS = [
+        ('C', 'Closed'), 
+        ('D', 'Developed'), 
+        ('InP', 'In progress'), 
+        ('InT', 'In testing'), 
+        ('N', 'New'), 
+        ('R', 'Rejected'), 
+        ('S', 'Scheduled')]
+    
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=TYPE_CHOICES)
+    status = models.CharField(max_length=255, choices=TYPE_STATUS)
+    assignee = models.CharField(max_length=255)
+    time_spent = models.DurationField(blank=True)
+    description = models.TextField()
+
+
+    def __str__(self):
+        txt = "Title => {}\n Status = {}"
+        return txt.format(self.title, self.status)
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Information
 
 def index(request):
     return HttpResponse("Hello. You're at tasks index.")
 
+def tasks_list(request):
+    tasks = Information.objects.all()
+    return render(request, 'tasks/tasks_list.html', {'tasks': tasks})
