4409d8fa61ac07eed1c2c8fc9b7a91f50ee6fc02
Evandro <evandrolrff@gmail.com>
2022-01-31 10:06:36 -0300

    armazenada secret key

--- tasksManagement/settings.py
+++ tasksManagement/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l%ojvxu3-lc)28$6g!nl))uhlgf21h=mrfuwy0b$fi=s$y=h(_'
+SECRET_KEY = open("SECRET_KEY", "r").read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
